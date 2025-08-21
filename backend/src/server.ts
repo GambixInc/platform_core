badc072bbeb5b525d2ee269baf91f9ca450bc747
Ravi Gupta <ravgupta20@gmail.com>
2025-08-21 13:15:13 -0400

    updated to add simple aws sdk import and set-up aws config

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,36 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import { S3Client } from "@aws-sdk/client-s3"
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.EXPRESS_PORT;
+
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID
+if (!AWS_ACCESS_KEY_ID) {
+  throw new Error('❌ AWS_ACCESS_KEY_ID environment variable is required');
+}
+
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY
+if (!AWS_SECRET_ACCESS_KEY) {
+  throw new Error('❌ AWS_SECRET_ACCESS_KEY environment variable is required');
+}
+
+const AWS_REGION = process.env.AWS_REGION
+if (!AWS_REGION) {
+  throw new Error('❌ AWS_REGION environment variable is required');
+}
+
+const s3Client = new S3Client({
+  region: AWS_REGION,
+  credentials: {
+    accessKeyId: AWS_ACCESS_KEY_ID!, 
+    secretAccessKey: AWS_SECRET_ACCESS_KEY!,
+  }
+});
+
 
 // Middleware
 app.use(
@@ -20,6 +45,10 @@ app.use(express.urlencoded({ extended: true }));
 // Basic route for testing
 app.get("/", (req, res) => {
   res.json({ message: "Backend server is running!" });
+});
+
+app.get("/aws", (req, res) =>{
+  res.json({message: "AWS Services endpoint"});
 });
 
 app.listen(PORT, () => {
